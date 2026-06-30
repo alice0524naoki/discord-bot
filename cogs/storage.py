5484f53dc7257c09d24dcef64694b1161c82dec3
alice0524naoki <alice.0524naoki@gmail.com>
2026-06-30 23:31:00 +0900

    Delete cogs/storage.py

--- cogs/storage.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-
-class Storage(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.bot.event_data = None
-
-async def setup(bot):
-    await bot.add_cog(Storage(bot))
